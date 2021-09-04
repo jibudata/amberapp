d009bccbf90abcc6b2e3ae27f50fef220b5fa64d
big-appled <87792656+big-appled@users.noreply.github.com>
2021-09-04 22:47:09 +0800

    feature: init crd and controller
    Update hook_resource.go

---
+++ api/v1alpha1/apphook_types.go
@@ -0,0 +1,96 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// AppHookSpec defines the desired state of AppHook
+type AppHookSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Name is a job for backup/restore/migration
+	Name string `json:"name"`
+	// AppProvider is the application identifier for different vendors, such as mysql
+	AppProvider string `json:"appProvider,omitempty"`
+	// Endpoint to connect the applicatio service
+	EndPoint string `json:"endPoint,omitempty"`
+	// OperationType is the operation executed in application
+	//+kubebuilder:validation:Enum=quiesce;unquiesce
+	OperationType string `json:"operationType,omitempty"`
+	// TimeoutSeconds is the timeout of operation
+	//+kubebuilder:validation:Minimum=0
+	TimeoutSeconds *int32 `json:"timeoutSeconds,omitempty"`
+	// Secret to access the application
+	Secret corev1.SecretReference `json:"secret,omitempty"`
+	// Other options
+	Params map[string]string `json:"params,omitempty"`
+}
+
+// AppHookStatus defines the observed state of AppHook
+//+kubebuilder:subresource:status
+type AppHookStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Phase string `json:"phase,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Age",type=date,JSONPath=.metadata.creationTimestamp
+//+kubebuilder:printcolumn:name="Created At",type=string,JSONPath=.metadata.creationTimestamp
+//+kubebuilder:printcolumn:name="Phase",type=string,JSONPath=.status.phase,description="Phase"
+
+// AppHook is the Schema for the apphooks API
+type AppHook struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   AppHookSpec   `json:"spec,omitempty"`
+	Status AppHookStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// AppHookList contains a list of AppHook
+type AppHookList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []AppHook `json:"items"`
+}
+
+const (
+	// quiesce operation
+	QUIESCE = "quiesce"
+	// unquiesce operation
+	UNQUIESCE = "unquiesce"
+)
+
+const (
+	HookCreated = "Created"
+	HookReady   = "Ready"
+)
+
+func init() {
+	SchemeBuilder.Register(&AppHook{}, &AppHookList{})
+}
