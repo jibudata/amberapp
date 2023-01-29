435533ae5b7c496179ed81ac532d8687bb700396
jerry <jerry@jibudata.com>
2023-01-29 14:59:11 +0800

    fix: correct go mod and fill the standalone support logic
    Signed-off-by: jerry <jerry@jibudata.com>

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@ module github.com/jibudata/amberapp
 go 1.19
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/lib/pq v1.10.3
 	github.com/onsi/ginkgo v1.16.5
-	github.com/onsi/gomega v1.25.0
+	github.com/onsi/gomega v1.18.1
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
 	go.mongodb.org/mongo-driver v1.7.3
+	golang.org/x/mod v0.3.1-0.20200828183125-ce943fd02449
 	k8s.io/api v0.21.2
 	k8s.io/apimachinery v0.21.2
 	k8s.io/client-go v0.21.2
@@ -27,20 +29,20 @@ require (
 	github.com/Azure/go-autorest/logger v0.2.0 // indirect
 	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/evanphx/json-patch v4.11.0+incompatible // indirect
 	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v0.4.0 // indirect
 	github.com/go-logr/zapr v0.4.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/google/uuid v1.1.2 // indirect
 	github.com/googleapis/gnostic v0.5.5 // indirect
@@ -57,7 +59,6 @@ require (
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.26.0 // indirect
 	github.com/prometheus/procfs v0.6.0 // indirect
-	github.com/redis/go-redis/v9 v9.0.0-rc.4
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
@@ -66,20 +67,20 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/crypto v0.0.0-20210220033148-5ea612d1eb83 // indirect
-	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/term v0.4.0 // indirect
-	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.6 // indirect
 	golang.org/x/time v0.0.0-20210611083556-38a9dc6acbc6 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.2.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	k8s.io/apiextensions-apiserver v0.21.2 // indirect
 	k8s.io/component-base v0.21.2 // indirect
 	k8s.io/kube-openapi v0.0.0-20210305001622-591a79e4bda7 // indirect
--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"golang.org/x/mod/semver"
@@ -23,9 +24,9 @@ const (
 	AOFOnly  BackupMethod = "aofonly"
 	None     BackupMethod = "none"
 
-	AppendOnly       RedisConfigCmdType = "appendonly"
-	Save             RedisConfigCmdType = "save"
-	AOFReWriteOption RedisConfigCmdType = "auto-aof-rewrite-percentage"
+	AppendOnly               RedisConfigCmdType = "appendonly"
+	Save                     RedisConfigCmdType = "save"
+	AutoAOFReWritePercentage RedisConfigCmdType = "auto-aof-rewrite-percentage"
 
 	ServerInfo      RedisInfoCmdType = "server"
 	PersistenceInfo RedisInfoCmdType = "persistence"
@@ -73,24 +74,60 @@ func (r *Redis) Connect() error {
 		return err
 	}
 
+	if !r.IsSupported() {
+		return fmt.Errorf("%s isn't supported yet", r.String())
+	}
+
 	log.Log.Info("Redis connected")
 	return nil
 }
 
 func (r *Redis) Quiesce() (*v1alpha1.QuiesceResult, error) {
-	if !r.IsSupported() {
-		return nil, nil
+	isAOFEnabled, err := r.isAOFEnabled()
+	if err != nil {
+		return nil, err
 	}
 
-	return nil, nil
+	snapshot, err := r.getSnapshotConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	preserved := make(map[string]string)
+
+	if isAOFEnabled {
+		preserved[string(AppendOnly)] = "yes"
+
+		percentage, err := r.getAutoAOFRewritePercentage()
+		if err != nil {
+			return nil, err
+		}
+
+		preserved[string(AutoAOFReWritePercentage)] = fmt.Sprintf("%d", percentage)
+	}
+
+	if snapshot != "" {
+		preserved[string(Save)] = snapshot
+	}
+
+	return &v1alpha1.QuiesceResult{
+		Redis: &v1alpha1.RedisResult{
+			QuiescePreservedConfig: preserved,
+		},
+	}, nil
 }
 
 func (r *Redis) Unquiesce() error {
-	if !r.IsSupported() {
-		return nil
-	}
 
 	return nil
+}
+
+func (r *Redis) String() string {
+	return fmt.Sprintf("redis instance %s with version: %d, topology: %s, backup method: %s",
+		r.config.Host,
+		r.version,
+		r.architecture,
+		r.mode)
 }
 
 func (r *Redis) getRedisVersion() error {
@@ -140,6 +177,81 @@ func (r *Redis) getRedisMode() error {
 	}
 
 	return nil
+}
+
+func (r *Redis) isAOFEnabled() (bool, error) {
+	v, err := r.rdb.ConfigGet(context.TODO(), string(AppendOnly)).Result()
+	if err != nil {
+		return false, err
+	}
+
+	if len(v) != 2 {
+		return false, fmt.Errorf("invalid result length: %#v from config get %s", v, AppendOnly)
+	}
+
+	result := v[1].(string)
+	if result != "yes" && result != "no" {
+		return false, fmt.Errorf("invalid result %s from %#v, must be yes or no", result, v)
+	}
+
+	return result == "yes", nil
+}
+
+func (r *Redis) getSnapshotConfig() (string, error) {
+
+	v, err := r.rdb.ConfigGet(context.TODO(), string(Save)).Result()
+	if err != nil {
+		return "", err
+	}
+
+	if len(v) != 2 {
+		return "", fmt.Errorf("invalid result length: %#v from config get %s", v, Save)
+	}
+
+	result, ok := v[1].(string)
+	if !ok {
+		return "", fmt.Errorf("failed to convert result %#v from config get %#v to string", result, v)
+	}
+
+	return result, nil
+}
+
+func (r *Redis) getAutoAOFRewritePercentage() (int, error) {
+
+	v, err := r.rdb.ConfigGet(context.TODO(), string(AutoAOFReWritePercentage)).Result()
+	if err != nil {
+		return -1, err
+	}
+
+	if len(v) != 2 {
+		return -1, fmt.Errorf("invalid result length: %#v from config get %s", v, AutoAOFReWritePercentage)
+	}
+
+	result, ok := v[1].(int)
+	if !ok {
+		return -1, fmt.Errorf("failed to convert result %#v from config get %#v to string", result, v)
+	}
+
+	return result, nil
+}
+
+func (r *Redis) isAOFRewriteInProgress() (bool, error) {
+	val, err := r.rdb.Info(context.TODO(), string(PersistenceInfo)).Result()
+	if err != nil {
+		return false, err
+	}
+
+	result := extractRedisInfoResult(val, "aof_rewrite_in_progress:")
+	if result == "" {
+		return false, fmt.Errorf("can't get aof_rewrite_in_progress from result %s", val)
+	}
+
+	inprogresFlag, err := strconv.Atoi(result)
+	if err != nil {
+		return false, fmt.Errorf("failed to convert %s to int flag from result %s", result, val)
+	}
+
+	return inprogresFlag == 1, nil
 }
 
 func extractRedisInfoResult(result string, prefix string) string {
